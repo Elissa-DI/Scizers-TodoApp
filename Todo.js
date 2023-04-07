c31b7f516f7ada3627abb559796837b0a5f4a842
Elissa <elissadusabe@gmail.com>
2023-04-07 16:48:06 +0200

    Done with all

--- Todo.js
+++ Todo.js
@@ -1,19 +1,18 @@
-// Array to store tasks
 var tasks = [];
+var users = [];
 
-// Function to add a user
 function addUser() {
     var username = document.getElementById("username").value;
     if (username !== "") {
-        // alert("User " + username + " added successfully.");
+        users.push({username: username});
         document.getElementById("username").value = "";
+        displayTasks();
+        document.getElementById("ren-username").textContent = "Username: " + username;
     } else {
         alert("Please enter a username.");
     }
 }
-
-// Function to add a task
-function addTask() {
+function addTask(e) {
     var taskName = document.getElementById("task-name").value;
     var dueDate = document.getElementById("due-date").value;
     if (taskName !== "" && dueDate !== "") {
@@ -21,32 +20,29 @@ function addTask() {
         document.getElementById("task-name").value = "";
         document.getElementById("due-date").value = "";
         displayTasks();
+        e.preventDefault();
     } else {
         alert("Please enter a task name and due date.");
     }
 }
-
-// Function to display tasks
 function displayTasks() {
     var taskList = document.getElementById("task-list");
+    var userList = document.getElementById("ren-username");
     taskList.innerHTML = "<tr><th>Task Name</th><th>Due Date</th><th>Status</th><th>Action</th></tr>";
+    userList.innerHTML = "<p></p>";
     for (var i = 0; i < tasks.length; i++) {
         var task = tasks[i];
+        var user = users[i];
         var row = document.createElement("tr");
-        row.innerHTML = "<td>" + task.name + "</td><td>" + task.dueDate + "</td><td class='" + task.status + "'>" + task.status + "</td><td><button onclick='deleteTask(" + i + ")'>Delete</button><button onclick='editTask(" + i + ")'>Edit</button><button onclick='changeTaskStatus(" + i + ")'>Mark " + (task.status === "pending" ? "completed" : "pending") + "</button></td>";
+        var userL = document.createElement("p");
+        row.innerHTML = "<td>" + task.name + "</td><td>" + task.dueDate + "</td><td class='" + task.status + "'>" + task.status + "</td><td><button onclick='deleteTask(" + i + ")' class='delete-btn'>Delete</button><button onclick='editTask(" + i + ")' class='edit-btn'>Edit</button><button onclick='changeTaskStatus(" + i + ")' class='mark-btn'>" + (task.status === "pending" ? "completed" : "pending") + "</button></td>";
         taskList.appendChild(row);
     }
 }
-
-// Function to delete a task
 function deleteTask(index) {
-    if (confirm("Are you sure you want to delete this task?")) {
         tasks.splice(index, 1);
         displayTasks();
     }
-}
-
-// Function to edit a task
 function editTask(index) {
     var task = tasks[index];
     var newTaskName = prompt("Enter a new name for this task:", task.name);
@@ -60,26 +56,42 @@ function editTask(index) {
     }
 }
 
-// Function to change the status of a task
 function changeTaskStatus(index) {
     var task = tasks[index];
     task.status = (task.status === "pending" ? "completed" : "pending");
     displayTasks();
 }
-
-// Function to filter tasks
 function filterTasks() {
-    var taskName = document.getElementById("filter-task-name").value.toLowerCase();
-    var status = document.getElementById("filter-status").value.toLowerCase();
-    var dueDate = document.getElementById("filter-due-date").value;
-    var filteredTasks = tasks.filter(function(task) {
-        return task.name.toLowerCase().includes(taskName) &&
-            (status === "" || task.status === status) &&
-            (dueDate === "" || task.dueDate === dueDate);
-    });
-    tasks = filteredTasks;
-    displayTasks();
-}
+    var filterTaskName = document.getElementById("filter-task-name").value.toLowerCase();
+    var filterStatus = document.getElementById("filter-status").value.toLowerCase();
+    var filterDueDate = document.getElementById("filter-due-date").value;
+    var taskTable = document.getElementById("task-list");
+    var rows = taskTable.getElementsByTagName("tr");
+    
+    for (var i = 1; i < rows.length; i++) {
+      var taskName = rows[i].getElementsByTagName("td")[0].textContent.toLowerCase();
+      var dueDate = rows[i].getElementsByTagName("td")[1].textContent;
+      var status = rows[i].getElementsByTagName("td")[2].textContent.toLowerCase();
+      
+      if (taskName.includes(filterTaskName) && status.includes(filterStatus) && (filterDueDate == "" || dueDate == filterDueDate)) {
+        rows[i].style.display = "";
+      } else {
+        rows[i].style.display = "none";
+      }
+    }
+  }
+displayTasks(filterTasks);
 
-// Display all tasks initially
-displayTasks();
+//Highlight searched task
+function highlightSearchedTask(taskName) {
+    var taskNameCells = document.getElementsByClassName("task-name");
+    for (var i = 0; i < taskNameCells.length; i++) {
+      var cell = taskNameCells[i];
+      var text = cell.textContent.toLowerCase();
+      if (text.includes(taskName)) {
+        cell.style.backgroundColor = "red";
+      } else {
+        cell.style.backgroundColor = "white";
+      }
+    }
+  }
